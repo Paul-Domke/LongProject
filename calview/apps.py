49736c143055408f89c9f2aa0745faefd60f70a0
heystingray <austin.gray15@ncf.edu>
2018-03-27 15:53:10 -0400

    Cleaning House

--- calview/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CalviewConfig(AppConfig):
-    name = 'calview'
--- input/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class InputConfig(AppConfig):
-    name = 'input'
