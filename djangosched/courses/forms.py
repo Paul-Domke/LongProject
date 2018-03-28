83d7a86d69b35b500854fbf47dc93166998bdc21
HuffleWitch <rebecca.phillips14@ncf.edu>
2018-03-28 16:00:03 -0400

    added comment to test gitignore

--- djangosched/courses/forms.py
+++ djangosched/courses/forms.py
@@ -5,3 +5,5 @@ class CreateCourse(forms.ModelForm):
 	class Meta:
 		model = models.Course 
 		fields = ['title', 'decription', 'slug', 'pref_day', 'pref_time', 'room']
+		#will add more in depth, specific fields of different types to make it easier to 
+		#input time and day options. 
