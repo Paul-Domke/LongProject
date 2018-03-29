afec4e55f683426f52de14dc3888b10ab078621d
HuffleWitch <rebecca.phillips14@ncf.edu>
2018-03-29 18:11:40 -0400

    updating the time/day selection
    changed the day time input to a drop down with the ucs time/day slot choices

--- djangosched/courses/forms.py
+++ djangosched/courses/forms.py
@@ -1,9 +1,11 @@
 from django import forms
 from . import models
+from courses.choices import *
 
 class CreateCourse(forms.ModelForm):
+	ucs_time_date = forms.ChoiceField(choices = UCS_TIME_CHOICES, required = True)
 	class Meta:
-		model = models.Course 
-		fields = ['title', 'decription', 'slug', 'pref_day', 'pref_time', 'room']
+		model = models.Course 		
+		fields = ['title', 'decription', 'slug', 'room']
 		#will add more in depth, specific fields of different types to make it easier to 
 		#input time and day options. 
--- djangosched/courses/models.py
+++ djangosched/courses/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
+from courses.choices import *
+
 
 # Create your models here.
 class Course(models.Model):
 	title = models.CharField(max_length = 100)
-	pref_time = models.CharField(max_length = 10)
 	decription = models.TextField()
+	ucs_time_date = models.CharField(choices = UCS_TIME_CHOICES, max_length = 10)
 	slug = models.SlugField()
 	date = models.DateTimeField(auto_now_add = True)
-	pref_day = models.CharField(max_length = 20)
 	professor = models.ForeignKey(User, default = None, on_delete = True)
 	room = models.CharField(max_length = 10)
 
