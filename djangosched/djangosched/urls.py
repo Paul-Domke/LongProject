0fdfda903e3e17d8a2c1d013c37c77cdb75c43c0
HuffleWitch <rebecca.phillips14@ncf.edu>
2018-03-30 11:44:07 -0400

    added a working "home" button

--- djangosched/djangosched/urls.py
+++ djangosched/djangosched/urls.py
@@ -3,11 +3,10 @@ from django.urls import path, include
 from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from courses import views as course_views
-from home import views as home_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_views.home_view, name = "home"),
+    path('', include('home.urls')),
     path('about/', views.about),
     path('courses/', include('courses.urls')),
     path('accounts/', include('accounts.urls')),
