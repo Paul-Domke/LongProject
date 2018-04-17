0e37f1b7bc40fcae9444687497173e0d7fbeb672
heystingray <austin.gray15@ncf.edu>
2018-04-17 18:22:17 -0400

    Added Instructor and Time fields to course list form

--- djangosched/courses/forms.py
+++ djangosched/courses/forms.py
@@ -8,3 +8,7 @@ class CreateCourse(forms.ModelForm):
 		fields = ['title', 'department', 'level', 'decription', 'slug', 'ucs_time_date1', 'ucs_time_date2', 'ucs_time_date3', 'room1', 'room2', 'room3']
 		#will add more in depth, specific fields of different types to make it easier to
 		#input time and day options.
+
+class FilterCourseList(forms.Form):
+	instructor = forms.ModelChoiceField(label='Instructor:', queryset=models.User.objects.all().order_by('username'), required=False)
+	time = forms.ChoiceField(label='Time:', choices=UCS_TIME_CHOICES, required=False)
