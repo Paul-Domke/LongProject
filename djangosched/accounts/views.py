b40fc1b409f7daa89d5c363504b0e1dafd879d4e
HuffleWitch <rebecca.phillips14@ncf.edu>
2018-05-02 17:19:14 -0400

    changed the welcome/home page

--- djangosched/accounts/views.py
+++ djangosched/accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, authenticate
 from django.contrib.auth import login, logout
 from accounts.forms import *
 
