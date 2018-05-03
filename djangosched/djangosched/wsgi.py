453440ed1e1a7b31c76d6a52b2e0a7be8eb8371a
heystingray <austin.gray15@ncf.edu>
2018-05-03 19:27:14 -0400

    Renaming shit here
    it never ends

--- djangosched/djangosched/wsgi.py
+++ djangosched/djangosched/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for coursesilver project.
+WSGI config for djangosched project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "coursesilver.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangosched.settings")
 
 application = get_wsgi_application()
