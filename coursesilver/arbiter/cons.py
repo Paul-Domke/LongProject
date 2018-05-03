b9ebef89ff8258cd4c8566f218c8adea96017521
heystingray <austin.gray15@ncf.edu>
2018-05-03 17:02:23 -0400

    cleanse my soul

--- coursesilver/arbiter/cons.py
+++ coursesilver/arbiter/cons.py
@@ -8,11 +8,11 @@ def all_cons(times):
         for val in vals:
             for val2 in vals[i:]:
                 if val['room'] == val2['room']:
-                    result += 10
-                if val['prof'] == val2['prof']:
-                    result += 10
-                if val['dept'] == val2['dept'] and val['level'] == val2['level']:
                     result += 1
+                if val['prof'] == val2['prof']:
+                    result += 1
+                if val['dept'] == val2['dept'] and val['level'] == val2['level']:
+                    result += 0.01
             i += 1
 
     tkeys = times.keys()
@@ -32,41 +32,3 @@ def all_cons(times):
                 break
 
     return result
-
-
-def con_nosameproftime(assign):
-    """ Takes assignment and determines whether it has a professor in 2 places at once """
-    result = 0
-    vals = list(assign.values())
-    i = 1
-    for val in vals:
-        for val2 in vals[i:]:
-            if not val2['time'].equals(val['time']):
-                break
-            if val['prof'] == val2['prof'] and val['time'].overlaps(val2['time']):
-                result += 100
-        i += 1
-    return result
-
-def con_level(assign):
-    """
-    Takes assignment values and determines if any two courses of same department
-    and level occur at same time
-    """
-    result = 0
-    vals = list(assign.values())
-    i = 1
-    for val in vals:
-        for val2 in vals[i:]:
-            if not val2['time'].equals(val['time']):
-                break
-            if val['dept'] == val2['dept'] and val['level'] == val2['level'] and val['time'].overlaps(val2['time']):
-                result += 10
-        i += 1
-    return result
-
-def con_classfits(assign):
-    pass
-
-def make_class_conflict_con(courseid1, courseid2):
-    pass
--- coursesilver/arbiter/sched.py
+++ coursesilver/arbiter/sched.py
@@ -6,90 +6,10 @@ import math
 from sortedcontainers import SortedDict
 
 def get_solution(pref):
-    """
-    This is the function to call, what this whole module is for.
-    It takes a python dictionary representing preferences, which must have
-    entries of the following form:
 
-    { COURSE_IDENTIFIER:{'prof':PROFESSOR_IDENTIFIER,
-                         'time':LIST_OF_TIMES,
-                         'room':LIST_OF_ROOMS,},}
-
-    It will return a solution also in the form of a dictionary:
-
-    { COURSE_IDENTIFIER:{'time':ASSIGNED_TIME,
-                         'room':ASSIGNED_ROOM},}
-
-    If there is no possible solution it will return the string 'FAILURE'
-    """
-    """
-    solution = ideal_solution(pref)
-
-    if solution == 'FAILURE':
-        solution = strict_solution(pref)
-    """
     solution = get_annealed(pref)
 
     return solution
-
-def ideal_solution(pref):
-    cons = [con_nosametimeplace, con_nosameproftime, con_level]
-    return make_solution({}, list(pref.keys()), build_domains(pref), cons)
-
-def strict_solution(pref):
-    cons = [con_nosametimeplace, con_nosameproftime]
-    return make_solution({}, list(pref.keys()), build_domains(pref), cons)
-
-def make_solution(assign, variables, domains, constraints):
-    #print('top', assign)
-    if iscomplete(assign, variables):
-        return assign
-
-
-    # Remove a variable var from variables
-    # pick variable using Most Constrained Variable
-    var = get_mcv(domains)
-    variables.remove(var)
-    #print('var', var)
-
-
-    # order domain values by Least Constraining Value
-    vals = sorted(domains.pop(var), key=lambda val:how_constraining(val, domains))
-
-    for value in vals:
-        #print('value',value)
-        newassign = copy.deepcopy(assign)
-        newassign[var] = value
-        #thisisok = False not in [constraint(newassign) for constraint in constraints]
-        # if var = value is consistent with assign according to the constraints then
-        thisisok = True
-        if thisisok:
-            #print('got to 1')
-            # Add var = value to assign
-            assign = newassign
-            newdomains = copy.deepcopy(domains)
-            # for uvar in variables (i.e., uvar an unassigned variable), uvar − − − var (i.e., uvar a neighbor of var in the constrained graph) do
-            #       Remove values for uvar from newdomains[uvar] that are inconsistent with assign
-            for uvar in variables:
-                #print('got to 2')
-                for uval in newdomains[uvar]:
-                    uassign = copy.deepcopy(newassign)
-                    uassign[uvar] = uval
-                    #print(uvar, uval, [constraint(uassign) for constraint in constraints])
-                    if sum([constraint(uassign) for constraint in constraints]) > 0:
-                        #print('got to 3')
-                        newdomains[uvar].remove(uval)
-                        #print(newdomains)
-
-            # if for all uvar ∈ variables, uvar − − − var, we have newdomains (uvar ) not empty then
-            if [] not in newdomains.values():
-                result = make_solution(newassign, variables, newdomains, constraints)
-                if result != 'FAILURE':
-                    return result
-
-            #Remove var = value from assign
-
-    return 'FAILURE'
 
 def get_annealed(pref):
     """
@@ -189,16 +109,6 @@ def mark_conflicts(assign, constraints):
                     val2['enemies'] += "<br>"+val['name']
         i += 1
 
-
-def iscomplete(assign, variables):
-    """
-    Determines whether assignment is a complete assignment. Returns bool.
-    """
-    for var in variables:
-        if var not in assign:
-            return False
-    return True
-
 def build_domains(pref):
     """
     Given a set of course preferences, generates complete domains of every
@@ -219,25 +129,6 @@ def build_domains(pref):
                                         'conflict':False,
                                         'enemies':''})
     return domains
-
-def get_mcv(domains):
-    mcv = None
-    smallest = 1000
-    for key in domains:
-        d = len(domains[key])
-        if d < smallest:
-            smallest = d
-            mcv = key
-    return mcv
-
-def how_constraining(val, domains):
-    out = 0
-    for domain in domains.values():
-        for other in domain:
-            if val['room'] == other['room'] or val['prof'] == other['prof']:
-                if val['time'].overlaps(other['time']):
-                    out += 1
-    return out
 
 def get_times_dict(pref):
     times = set()
