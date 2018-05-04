7ce1056f54039807cd9c516e1f5e61f1353149b8
heystingray <austin.gray15@ncf.edu>
2018-05-03 23:47:17 -0400

    OG 100 10 yo yoy o

--- djangosched/arbiter/cons.py
+++ djangosched/arbiter/cons.py
@@ -8,11 +8,11 @@ def all_cons(times):
         for val in vals:
             for val2 in vals[i:]:
                 if val['room'] == val2['room']:
-                    result += 10
+                    result += 100
                 if val['prof'] == val2['prof']:
-                    result += 10
+                    result += 100
                 if val['dept'] == val2['dept'] and val['level'] == val2['level']:
-                    result += 1
+                    result += 10
             i += 1
 
     tkeys = times.keys()
@@ -23,11 +23,11 @@ def all_cons(times):
                 for val in times[time]:
                     for val2 in times[time2]:
                         if val['room'] == val2['room']:
-                            result += 10
+                            result += 100
                         if val['prof'] == val2['prof']:
-                            result += 10
+                            result += 100
                         if val['dept'] == val2['dept'] and val['level'] == val2['level']:
-                            result += 1
+                            result += 10
             else:
                 break
 
